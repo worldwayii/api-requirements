0c94289e02bc0a634acfef6583e2065b68c4863e
Chuwkwuma Elui <eluichuma@gmail.com>
2022-09-20 00:33:55 +0100

    Refactor query string fetch

--- src/app/Domain/Product/Controllers/ProductController.php
+++ src/app/Domain/Product/Controllers/ProductController.php
@@ -5,14 +5,15 @@ namespace App\Domain\Product\Controllers;
 use App\Domain\Product\Model\Product;
 use App\Domain\Product\Resource\ProductResource;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function index($category = null)
+    public function index(Request $request)
     {
-        if($category == 'insurance'){
+        if($request->input('category') == "insurance" ){
             return $this->successResponse(
-                ProductResource::collection(Product::where('category', $category)->get())
+                ProductResource::collection(Product::where('category', $request->input('category'))->get())
             );
         }
 
